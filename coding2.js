da4887fbbed4ae8dfae1425b3cbd2f50ea76e2e4
Bryan Leanoe <bryan.leano@gmail.com>
2018-07-29 16:54:27 -0600

    do coding challenge 2

--- coding2.js
+++ coding2.js
@@ -1,3 +1,15 @@
 /****************************
  * CODING CHALLENGE 2
  */
+
+var teamScoreJohn = (105 + 134 + 98) / 3;
+var teamScoreMike = (120 + 89 + 115) / 3;
+
+
+if (teamScoreJohn > teamScoreMike) {
+  console.log('Team John wins with a score of ' + teamScoreJohn);
+} else if (teamScoreMike > teamScoreJohn) {
+  console.log('Team Mike wins with a score of ' + teamScoreMike);
+} else {
+  console.log('It\'s a tie at a score of ' + teamScoreJohn); 
+}

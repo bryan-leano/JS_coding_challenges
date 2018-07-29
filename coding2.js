95fe8e9d6290fa5097f567b82de595d83b68053b
Bryan Leanoe <bryan.leano@gmail.com>
2018-07-29 17:36:37 -0600

    add team Mary in coding challenge 2

--- coding2.js
+++ coding2.js
@@ -4,12 +4,15 @@
 
 var teamScoreJohn = (105 + 134 + 98) / 3;
 var teamScoreMike = (120 + 89 + 115) / 3;
+var teamScoreMary = (197 + 134 + 110) / 3;
 
-
-if (teamScoreJohn > teamScoreMike) {
+if (teamScoreJohn > teamScoreMike && teamScoreJohn > teamScoreMary) {
   console.log('Team John wins with a score of ' + teamScoreJohn);
-} else if (teamScoreMike > teamScoreJohn) {
+} else if (teamScoreMike > teamScoreJohn && teamScoreMike > teamScoreMary) {
   console.log('Team Mike wins with a score of ' + teamScoreMike);
-} else {
+} else if (teamScoreMary > teamScoreJohn && teamScoreMary > teamScoreMike) {
+  console.log('Team Mary wins with a score of ' + teamScoreMary);
+}
+  else {
   console.log('It\'s a tie at a score of ' + teamScoreJohn); 
 }
